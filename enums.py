2edce16e3c84d7020b3191ee6beba570eede0199
Bradley Justice <bradjustice97@gmail.com>
2022-06-22 19:32:05 -0700

    testing second commit

---
+++ enums.py
@@ -0,0 +1,12 @@
+class Player:
+    PLAYER_1 = 'white'
+    PLAYER_2 = 'black'
+    EMPTY = -9
+    PIECES = ['white_r', 'white_n', 'white_q', 'white_k', 'white_p',
+              'black_r', 'black_n', 'black_q', 'black_k', 'black_p']
+    UNITS = ['white_recon', 'black_recon', 'black_tank', 'white_infantry', 'black_infantry', 'white_king', 'white_queen','black_king', 'black_queen', 'white_tank', 'black_tank', 'white_artillery', 'black_artillery']
+
+
+
+    # PIECES = ['white_r', 'white_n', 'white_b', 'white_q', 'white_k', 'white_p',
+    #           'black_r', 'black_n', 'black_b', 'black_q', 'black_k', 'black_p']
