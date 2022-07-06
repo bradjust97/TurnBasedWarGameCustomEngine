6c69a638f8d489554b66429e29fed4e3c4803662
Bradley Justice <bradjustice97@gmail.com>
2022-07-05 22:48:39 -0700

    cleaned up bfs alg

--- movement_engine.py
+++ movement_engine.py
@@ -16,14 +16,11 @@ def make_movement_diamond(movement):
     return diamond
 
 # technically movement and row/col not needed if u query the diamond
-# TODO probably can remove moveAttacks in the future
 def blocked_movement_diamond(movement, row_number, col_number, game_state):
     checked = [[row_number,col_number]]
-    moveAttacks = []
     M = movement
     m = 0
-    (DFSList, moveAttacks) = DFS_Traverse_Diamond(checked, M, m, game_state, moveAttacks)
-    #total_moves = DFSList + moveAttacks
+    DFSList = BFS_Traverse_Diamond(checked, M, m, game_state)
     # remove dupes
     listOfLists = [list(t) for t in set(tuple(element) for element in DFSList)]
     # turn list of list to list of tuples
@@ -31,49 +28,27 @@ def blocked_movement_diamond(movement, row_number, col_number, game_state):
     diamond.append((row_number, col_number))
     return diamond
 
-# # technically movement and row/col not needed if u query the diamond
-# def blocked_movement_diamond(movement, row_number, col_number, game_state):
-#     checked = [[row_number,col_number]]
-#     moveAttacks = []
-#     M = movement
-#     m = 0
-#     (DFSList, moveAttacks) = DFS_Traverse_Diamond(checked, M, m, game_state, moveAttacks)
-#     total_moves = DFSList + moveAttacks
-#     # remove dupes
-#     listOfLists = [list(t) for t in set(tuple(element) for element in total_moves)]
-#     # turn list of list to list of tuples
-#     return [tuple(l) for l in listOfLists]
-
-def DFS_Traverse_Diamond(checked, M, m, game_state, moveAttacksInput):
-    moveAttacks = moveAttacksInput.copy()
+def BFS_Traverse_Diamond(checked, M, m, game_state):
     newC = checked.copy()
     for square in checked:
         # check to right
         if(game_state.is_empty(square[0]+1, square[1]) and [square[0]+1, square[1]] not in checked):
             newC.append([square[0]+1, square[1]])
-        elif(game_state.is_valid_piece(square[0]+1, square[1]) and [square[0]+1, square[1]] not in checked):
-            moveAttacks.append([square[0]+1, square[1]])
 
         # check below
         if(game_state.is_empty(square[0], square[1]+1) and [square[0], square[1]+1] not in checked):
             newC.append([square[0], square[1]+1])
-        elif(game_state.is_valid_piece(square[0], square[1]+1) and [square[0], square[1]+1] not in checked):
-            moveAttacks.append([square[0], square[1]+1])
         
         # check left
         if(game_state.is_empty(square[0]-1, square[1]) and [square[0]-1, square[1]] not in checked):
             newC.append([square[0]-1, square[1]])
-        elif(game_state.is_valid_piece(square[0]-1, square[1]) and [square[0]-1, square[1]] not in checked):
-            moveAttacks.append([square[0]-1, square[1]])
 
         # check above
         if(game_state.is_empty(square[0], square[1]-1) and [square[0], square[1]-1] not in checked):
             newC.append([square[0], square[1]-1])
-        if(game_state.is_valid_piece(square[0], square[1]-1) and [square[0], square[1]-1] not in checked):
-            moveAttacks.append([square[0], square[1]-1])
         
     m += 1
     if(m == M):
-        return (newC, moveAttacks) #TODO return here two arrays, one that is peacefuls and one that is hits. make sure to only keep DFS on the peaceful one
+        return newC #TODO return here two arrays, one that is peacefuls and one that is hits. make sure to only keep DFS on the peaceful one
     else:
-        return DFS_Traverse_Diamond(newC,M,m,game_state, moveAttacks)
+        return BFS_Traverse_Diamond(newC,M,m,game_state)
