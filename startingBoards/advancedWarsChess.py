a8f4b164360e3f2c226a1526ea9bf7a27cb1d72c
Bradley Justice <bradjustice97@gmail.com>
2022-08-21 14:45:43 -0700

    added unit arts

--- startingBoards/advancedWarsChess.py
+++ startingBoards/advancedWarsChess.py
@@ -8,7 +8,8 @@ from units.Knight import Knight
 from units.Footman import Footman
 import random
 
-white_knight_1 = Knight(0, 0, Player.PLAYER_1)
+white_knight_1 = Knight(14, 0, Player.PLAYER_1)
+white_knight_2 = Knight(15, 0, Player.PLAYER_1)
 white_catapult_1 = Catapult(0, 1, Player.PLAYER_1)
 white_scoutCavalry_1 = ScoutCavalry(0, 2, Player.PLAYER_1)
 white_king = King(0, 3, Player.PLAYER_1)
@@ -17,13 +18,13 @@ white_footman_2 = Footman(1, 1, Player.PLAYER_1)
 white_footman_3 = Footman(1, 2, Player.PLAYER_1)
 white_footman_4 = Footman(1, 3, Player.PLAYER_1)
 white_footman_5 = Footman(1, 4, Player.PLAYER_1)
-white_pieces = [white_knight_1, white_footman_1] #, white_catapult_1, white_scoutCavalry_1,
+white_pieces = [white_footman_2, white_footman_1, white_knight_1, white_knight_2] #, white_catapult_1, white_scoutCavalry_1,
 # white_pieces = [white_knight_1, white_catapult_1, white_scoutCavalry_1,
 #                          white_king, white_footman_1, white_footman_2, white_footman_3, white_footman_4, white_footman_5] #white_queen,
 
 # Initialize Black Pieces
-black_knight_2 = Knight(15, 15, Player.PLAYER_2)
-black_knight_3 = Knight(8, 11, Player.PLAYER_2)
+black_knight_2 = Knight(0, 15, Player.PLAYER_2)
+black_knight_3 = Knight(0, 14, Player.PLAYER_2)
 black_catapult_2 = Catapult(15, 14, Player.PLAYER_2)
 black_scoutCavalry_2 = ScoutCavalry(15, 13, Player.PLAYER_2)
 black_king = King(15, 11, Player.PLAYER_2)
@@ -32,7 +33,7 @@ black_footman_5 = Footman(14, 12, Player.PLAYER_2)
 black_footman_6 = Footman(14, 13, Player.PLAYER_2)
 black_footman_7 = Footman(14, 14, Player.PLAYER_2)
 black_footman_8 = Footman(14, 15, Player.PLAYER_2)
-black_pieces = [ black_knight_2]
+black_pieces = [ black_footman_4, black_footman_5, black_knight_2, black_knight_3]
 # black_pieces = [ black_knight_2, black_catapult_2, black_scoutCavalry_2,
 #                          black_king, black_footman_4,
 #                         black_footman_5,
@@ -40,26 +41,6 @@ black_pieces = [ black_knight_2]
 
 board = [[Player.EMPTY for x in range(SquareBoard.DIMENSIONS)] for y in range(SquareBoard.DIMENSIONS)] 
 
-# board[8][10] = Player.WALL
-# board[8][9] = Player.WALL
-# board[8][8] = Player.WALL
-# board[8][6] = Player.WALL
-# board[8][5] = Player.WALL
-# board[8][4] = Player.WALL
-
-# board[7][10] = Player.WALL
-# board[7][9] = Player.WALL
-# board[7][8] = Player.WALL
-# board[7][6] = Player.WALL
-# board[7][5] = Player.WALL
-# board[7][4] = Player.WALL
-
-# board[6][10] = Player.WALL
-# board[6][9] = Player.WALL
-# board[6][8] = Player.WALL
-# board[6][6] = Player.WALL
-# board[6][5] = Player.WALL
-# board[6][4] = Player.WALL
 
 for piece in white_pieces:
     board[piece.get_row_number()][piece.get_col_number()] = piece
